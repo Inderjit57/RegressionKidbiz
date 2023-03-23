b7c83eb2739d15c1b5b691bf4c9bb1b9723dff32
Inderjit57 <singhinderjit57@yahoo.com>
2023-03-23 11:23:07 -0400

    Class name updated

--- src/test/java/com/Kidbiz/CreateUsers/CanvasNewUserTest.java
+++ src/test/java/com/Kidbiz/CreateUsers/CanvasNewUserTest.java
@@ -3,47 +3,53 @@ package com.Kidbiz.CreateUsers;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.KidbizSSO.BasePackage.BaseClass;
 import com.KidbizSSO.Method.Canvas.CanvasAdminHomepage;
 import com.KidbizSSO.Method.Canvas.CanvasLoginPage;
-import com.KidbizSSO.Method.Canvas.CanvasSearchPage;
+import com.KidbizSSO.Method.Canvas.CanvasCreateUserPage;
 import com.KidbizSSO.Method.Schoology.Schoology;
+import com.KidbizSSO.Util.WriteIntoExcel;
 
-public class CanvasNewUserTest extends BaseClass{
-	
+public class CanvasNewUserTest extends BaseClass {
+
 	CanvasLoginPage canvasLoginPage;
 	CanvasAdminHomepage adminHomepage;
-	CanvasSearchPage canvasSearchPage;
-	
+	CanvasCreateUserPage canvasCreateUserPage;
+	SoftAssert softAssert = new SoftAssert();
+
 	@BeforeMethod
 	public void initialisation() {
 		browserInitialization();
 		wd.get(properties.getProperty("canvasUrl"));
-		
-		canvasLoginPage =new CanvasLoginPage();
+
+		canvasLoginPage = new CanvasLoginPage();
 	}
-	
-	@Test
-	public void createNewCanvasTeacherTest() {
-		//Canvas login page
+
+	@Test()
+	public void createNewCanvasTeacherTest() throws Exception {
+		System.out.println("Current URL: " + wd.getCurrentUrl());
+		softAssert.assertEquals(wd.getCurrentUrl(), properties.getProperty("canvasUrl"),
+				"Not a valid Page. Expecting Canvas Login Page");
+		// Canvas login page
 		canvasLoginPage.inputEmail();
 		canvasLoginPage.inputPassword();
-		adminHomepage= canvasLoginPage.clickLogin();
-		
-		//Click on Admin button
+		adminHomepage = canvasLoginPage.clickLogin();
+
+		// Click on Admin button
 		adminHomepage.clickAdmin();
-		
+
 		// click on Actively Learn
-		canvasSearchPage= adminHomepage.clickOnAL();
-		canvasSearchPage.clickPeople();
-		canvasSearchPage.clickpeopleForm();
-		canvasSearchPage.fillForm();
-		
-		
+		canvasCreateUserPage = adminHomepage.clickOnAL();
+		canvasCreateUserPage.clickPeople();
+//		softAssert.assertEquals(canvasCreateUserPage.getSearchBtn(),"Search people...","Search element is not present" );
+		canvasCreateUserPage.clickpeopleForm();
+		canvasCreateUserPage.fillForm();
+
 	}
-	
-	@AfterMethod (enabled = false)
+
+	@AfterMethod(enabled = true)
 	public void tearDown() {
 		wd.quit();
 	}
