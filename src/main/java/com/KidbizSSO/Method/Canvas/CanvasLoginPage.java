a1a9619d99c418ec90389b07c50ca89e5858b25c
Inderjit57 <singhinderjit57@yahoo.com>
2023-03-23 11:22:28 -0400

    added comment on the top for page description

--- src/main/java/com/KidbizSSO/Method/Canvas/CanvasLoginPage.java
+++ src/main/java/com/KidbizSSO/Method/Canvas/CanvasLoginPage.java
@@ -5,34 +5,41 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.KidbizSSO.BasePackage.BaseClass;
+import com.KidbizSSO.Util.Utils;
 
 public class CanvasLoginPage extends BaseClass {
+
+	/*
+	 * Canvas Login page
+	 */
+
 	public CanvasLoginPage() {
 		// TODO Auto-generated constructor stub
 		PageFactory.initElements(wd, this);
 	}
-	
-	//Email
+
+	// Email
 	@FindBy(css = "#pseudonym_session_unique_id")
 	WebElement email;
 
 	public void inputEmail() {
-		email.sendKeys(properties.getProperty("emailCanvas"));
+		Utils.sendData(email, properties.getProperty("emailCanvas"));
 	}
 
-	//Password
+	// Password
 	@FindBy(css = "#pseudonym_session_password")
 	WebElement password;
 
 	public void inputPassword() {
-		password.sendKeys(properties.getProperty("passCanvas"));
+		Utils.sendData(password, properties.getProperty("passCanvas"));
 	}
-	
-	//LoginButton
+
+	// LoginButton
 	@FindBy(css = "#login_form div div button[class='Button Button--login']")
 	WebElement loginBtn;
+
 	public CanvasAdminHomepage clickLogin() {
-		loginBtn.click();
+		Utils.clickOnElement(loginBtn);
 		return new CanvasAdminHomepage();
 	}
 
