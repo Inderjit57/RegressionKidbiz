5abaeabfa37302988e73c3e9330465aaf847635e
Inderjit57 <singhinderjit57@yahoo.com>
2023-03-30 17:16:58 -0400

    in progress

--- src/main/java/com/KidbizSSO/Method/OLP/AdminAccountHomepage.java
+++ src/main/java/com/KidbizSSO/Method/OLP/AdminAccountHomepage.java
@@ -5,17 +5,32 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.KidbizSSO.BasePackage.BaseClass;
+import com.KidbizSSO.Util.Utils;
 
-public class AdminAccountHomepage extends BaseClass{
+public class AdminAccountHomepage extends BaseClass {
+
+	/*
+	 * OLP/MH - Admin account landing page
+	 */
 	public AdminAccountHomepage() {
 
 		// TODO Auto-generated constructor stub
 		PageFactory.initElements(wd, this);
 	}
-	
+
 	@FindBy(css = "div[class='dropdown ng-tns-c109-0']")
-	WebElement dropdown;
-	
-	
+	WebElement dropdownMenu;
+
+	@FindBy(css = "button[aria-label='administrator']")
+	WebElement administratorBtn;
+
+	public void clickDropdown() {
+		Utils.clickOnElement(dropdownMenu);
+	}
+
+	public AdministratorPage clickAdministrator() {
+		Utils.actionClick(dropdownMenu);
+		return new AdministratorPage();
+	}
 
 }
