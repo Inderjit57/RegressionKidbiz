fb4a00c0a323eb482b606d430ebdccaf21feec6d
Inderjit57 <singhinderjit57@yahoo.com>
2023-04-22 20:32:17 -0400

    Google OnDemand User creation test Page

---
+++ src/test/java/com/Kidbiz/GoogleOnDemandTest/GoogleOnDemandNewUserTest.java
@@ -0,0 +1,48 @@
+package com.Kidbiz.GoogleOnDemandTest;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.KidbizSSO.BasePackage.BaseClass;
+import com.KidbizSSO.Method.GoogleOnDemand.GoogleAdminHomepage;
+import com.KidbizSSO.Method.GoogleOnDemand.GoogleAdminSignInPage;
+import com.KidbizSSO.Method.GoogleOnDemand.GoogleUserProfilePage;
+
+public class GoogleOnDemandNewUserTest extends BaseClass {
+	GoogleAdminSignInPage googleAdminSignInPage;
+	GoogleAdminHomepage googleAdminHomepage;
+	GoogleUserProfilePage googleUserProfilePage;
+
+	@BeforeMethod
+	public void initialisation() {
+		browserInitialization();
+		wd.get(properties.getProperty("googleOnDemandUrl"));
+
+		googleAdminSignInPage = new GoogleAdminSignInPage();
+	}
+
+	@Test
+	public void createNewUSerGoogleOD() throws Exception {
+
+		googleAdminSignInPage.inputEmail();
+		googleAdminSignInPage.inputPassword();
+
+		googleAdminHomepage = googleAdminSignInPage.clickNext();
+		googleAdminHomepage.clickAddUserBtn();
+		googleAdminHomepage.inputInfo();
+		googleAdminHomepage.clickAddNewUserBtn();
+		googleAdminHomepage.getNewUserInfo();
+
+		googleUserProfilePage = googleAdminHomepage.searchForUser();
+		googleUserProfilePage.addUserToGroup();
+		googleUserProfilePage.resetUserPassword();
+		
+	}
+
+	@AfterMethod(enabled = false)
+	public void tearDown() {
+		wd.quit();
+	}
+
+}
